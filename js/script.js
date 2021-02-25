7919ebee2764eeddc70eb7ae4a7beaa66adeb42f
Srdan Cvijetic <scwebdevsc@gmail.com>
2021-02-25 01:01:23 +0100

    Update for Portfolio website

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ function hideMenu() {
   }
 }
 window.addEventListener("scroll", hideMenu);
+/* NAVBAR CHANGE ON SCROLL */
+document.addEventListener('scroll', () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 100 ) {
+    header.style.backgroundColor = 'rgb(206, 212, 218)';
+    header.style.borderBottom = "1px solid black";
+  } else {
+    header.style.backgroundColor = 'transparent'
+    header.style.borderBottom = 'none'
+  }
+});
